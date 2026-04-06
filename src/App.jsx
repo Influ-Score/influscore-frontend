9f7e8c83753aa874fed4455b2ec323b7c30e5cf9
NIKIRaw <73019582+Nitikarawat@users.noreply.github.com>
2026-04-06 20:56:14 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+      <div className="ticks px-5">Nitika</div>
+
+       </>
+  )
+}
+
+export default App
